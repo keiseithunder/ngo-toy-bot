7d307c89de2870f1c4dac0e7436441d9a5f5637e
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2020-11-12 21:33:10 +0700

    Add more condition

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
 
 require('dotenv').config()
 if (process.env.LOGIN_TOKEN) {
-    LOGIN_TOKEN = process.env.LOGIN_TOKEN
+	LOGIN_TOKEN = process.env.LOGIN_TOKEN
 }
 
 client.login(LOGIN_TOKEN)
@@ -11,10 +11,10 @@ client.login(LOGIN_TOKEN)
 client.on('message', async message => {
     if (message.author.username.toLowerCase().includes('toy')) {
         const toyMsg = message.content
-        if (toyMsg.includes('pornhub') || toyMsg.includes('youjizz')) {
+        if (toyMsg.includes('pornhub') || toyMsg.includes('youjizz')|| toyMsg.includes('พลัม')) {
             console.log(message.author.username, toyMsg)
             let toyRole = message.guild.roles.cache.find(r => r.name === 'ไอหน้าโง่ทอยโดน')
-            let toy = message.member
+			let toy = message.member
             try {
                 await message.delete({ timeout: 3000 })
             } catch (error) {
